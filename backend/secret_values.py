3d3f0ccd75eb6527e5346d34b4fc20453ef28f52
thienantran010 <96504081+thienantran010@users.noreply.github.com>
2023-10-29 16:19:58 -0400

    Update secret_values.py

--- backend/secret_values.py
+++ backend/secret_values.py
@@ -1,4 +1,1 @@
-mongodb_uri = "mongodb+srv://crownmobileapp5:sparkinnovationfellowship@cluster0.qwvh4zn.mongodb.net/?retryWrites=true&w=majority"
-rainforest_api_key = "FA8A74B3D30C41B28F402D3E7992788E"
-google_search_api_key = "AIzaSyCHXQkfyOqHc8boHQa4Xi-e-FTnu57TN9k"
-google_search_engine_id = "c7fd47316279d4c2d"
+
