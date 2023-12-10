fd24286d25977e1682abeeacad96d829caef1361
Favor Wariboko <66702423+FavorWa@users.noreply.github.com>
2023-12-10 18:43:33 -0500

    Update secrets.ts

--- frontend/secrets.ts
+++ frontend/secrets.ts
@@ -1,3 +1,5 @@
-const BACKEND_BASE_IOS = "http://localhost:8000";
-const BACKEND_BASE_ANDROID = "http://10.0.2.2:8000";
-export {BACKEND_BASE_IOS, BACKEND_BASE_ANDROID};
+const BACKEND_DEV_IOS = "http://localhost:8000";
+const BACKEND_DEV_AND = "http://10.0.2.2:8000";
+const BACKEND_PROD = "https://crown-production-eb1f.up.railway.app/";
+var isProd = true;
+export {BACKEND_DEV_IOS, BACKEND_DEV_AND, BACKEND_PROD, isProd};
